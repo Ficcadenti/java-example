b8bf544b3a3eb931135812fb07735ae3af031b7f
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2017-12-14 13:19:17 +0100

    Modificato servizio rest per 'Access-Control-Allow-Origin'

--- REST/RESTFull_serv_01/src/it/raffo/restfull/testRESTService.java
+++ REST/RESTFull_serv_01/src/it/raffo/restfull/testRESTService.java
@@ -118,7 +118,10 @@ public class testRESTService {
 		}
 
 		// return HTTP response 200 in case of success
-		return Response.status(200).entity(crunchifyBuilder.toString()).build();
+		return Response.status(200).entity(crunchifyBuilder.toString())
+				.header("Access-Control-Allow-Origin", "*")
+	            .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD")
+				.build();
 	}
 
 	@GET
