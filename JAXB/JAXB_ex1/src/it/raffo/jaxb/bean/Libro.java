8f4c1b51b8528eef9faa68bde844e9316404cd64
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2017-12-22 13:36:18 +0100

    Il primo esempio su JAXB

---
+++ JAXB/JAXB_ex1/src/it/raffo/jaxb/bean/Libro.java
@@ -0,0 +1,46 @@
+package it.raffo.jaxb.bean;
+
+import java.io.Serializable;
+import java.lang.Long;
+import java.lang.String;
+
+/**
+ * Entity implementation class for Entity: Libro
+ *
+ */
+
+public class Libro implements Serializable {
+
+	   
+	private Long id;
+	private String autore;
+	private String titolo;
+	private static final long serialVersionUID = 1L;
+
+	public Libro() {
+		super();
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}   
+	public String getAutore() {
+		return this.autore;
+	}
+
+	public void setAutore(String autore) {
+		this.autore = autore;
+	}   
+	public String getTitolo() {
+		return this.titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+   
+}
