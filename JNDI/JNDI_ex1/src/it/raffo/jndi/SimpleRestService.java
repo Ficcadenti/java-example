7a1684c94979d7e4ebc25abf1690c746e8c01342
Raffaele Ficcadenti <r.ficcadenti@exolab.it>
2018-03-06 11:11:25 +0100

    Add lombok.jar

--- JNDI/JNDI_ex1/src/it/raffo/jndi/SimpleRestService.java
+++ JNDI/JNDI_ex1/src/it/raffo/jndi/SimpleRestService.java
@@ -1,23 +1,31 @@
 package it.raffo.jndi;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.annotation.Resource;
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @RequestScoped
 @Path("/")
-public class SimpleRestService {
+public class SimpleRestService
+{
 
+	@Getter
+	@Setter
+	private int	test;
 
 	@Resource(lookup = "java:global/mystring")
-	String miaString;
+	String		miaString;
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String test() {
-		return miaString;
+	public String test()
+	{
+		return this.miaString;
 	}
 }
