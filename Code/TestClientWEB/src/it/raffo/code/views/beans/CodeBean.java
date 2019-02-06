5ae9f26bd3034c502b96ba8f2cb2ed4813bd39e6
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2019-02-06 16:53:37 +0100

    Fix code

--- Code/TestClientWEB/src/it/raffo/code/views/beans/CodeBean.java
+++ Code/TestClientWEB/src/it/raffo/code/views/beans/CodeBean.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import org.apache.log4j.Logger;
 
@@ -20,9 +22,21 @@ public class CodeBean implements Serializable
 	 */
 	private static final long	serialVersionUID	= -7483870296607162673L;
 	private static final Logger	log					= Logger.getLogger(CodeBean.class);
+	private String				msg					= null;
 
 	@EJB
 	private TestProducer		testProducer;
+
+	public void addMessage(String summary)
+	{
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", summary);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public String getMsg()
+	{
+		return this.msg;
+	}
 
 	@PostConstruct
 	public void init()
@@ -34,12 +48,19 @@ public class CodeBean implements Serializable
 		if (this.testProducer != null)
 		{
 			log.info("-----------------> sendMsg()");
-			this.testProducer.putOnQueue("ciaone");
+			this.testProducer.putOnQueue(this.getMsg());
+			this.addMessage("Messaggio inviato alla coda!!!!");
+			this.setMsg(null);
 		}
 		else
 		{
 			log.info("EJB Injection is null");
 		}
+	}
+
+	public void setMsg(String msg)
+	{
+		this.msg = msg;
 	}
 
 }
